9b1e293c0efe8d8657821c0fdf33506d54c28831
MartaRodriguezOnis <marta.ronis@gmail.com>
2022-09-05 11:23:32 +0200

    Add fetch movies

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-
+const movieData = require('./data/movies.json');
 // create and config server
 const server = express();
 server.use(cors());
@@ -10,4 +10,12 @@ server.use(express.json());
 const serverPort = 4000;
 server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
+});
+server.get('/movies', (req, resp) => {
+  console.log(req.query);
+  // resp.json(movieData);
+  const genderFilterParam = movieData.filter(
+    (movie) => movie.gender === req.query.gender
+  );
+  resp.json(genderFilterParam);
 });
